30524ea014ac045622eaeb05a73306b6c1747e0c
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-08-04 11:38:53 +0330

    Do signup signin basic functionality

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+import DontHaveAnAccount from "@/components/form/DontHaveAnAccount";
+import SignIn from "@/components/form/SignIn";
+import { Box } from "@mui/system";
 import React from "react";
 
 export default function page() {
-  return <div>sign in page</div>;
+  return (
+    <Box position={"relative"} right={"230px"}>
+      <SignIn />
+      <DontHaveAnAccount />
+    </Box>
+  );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,5 +1,11 @@
+import SignUp from "@/components/form/SignUp";
+import { Box } from "@mui/material";
 import React from "react";
 
 export default function page() {
-  return <div>sign up page</div>;
+  return (
+    <Box position={"relative"} right={"230px"}>
+      <SignUp />
+    </Box>
+  );
 }
