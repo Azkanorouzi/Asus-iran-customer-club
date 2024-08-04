5e8064a8259b40506f0b5c5db50b342e1a229a37
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-08-04 11:39:17 +0330

    Update middleware filters

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,4 +3,4 @@
 
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/extra", "/dashboard"] };
+export const config = { matcher: ["/transactions", "/customers"] };
