b85b71538ce787e827fd3c874ea7df8dddb4e1de
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-08-05 17:33:17 +0330

    Add customer slug

--- src/app/(dashboard)/customers/[slug]/page.tsx
+++ src/app/(dashboard)/customers/[slug]/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { useStore } from "@/lib/store";
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import Link from "next/link";
 import React from "react";
 
 export default function page({
@@ -8,18 +10,39 @@ export default function page({
 }: {
   params: { slug: string };
 }) {
-  const transactios = useStore((state) => state.transactions);
-  const targetTransaction = transactios.filter(
-    (transaction) => transaction.id === slug,
+  const customers = useStore((state) => state.customers);
+  const transactions = useStore((state) => state.transactions);
+  console.log(slug);
+  const customerTarget = customers.filter(
+    (customer) => customer.id === slug,
   )[0];
   return (
     <Box width={"100vw"} maxWidth={"950px"}>
       {" "}
       <Box component={"section"}>
         <Typography variant="h2" component="h1">
-          {targetTransaction.id}
+          id: {customerTarget.id} name: {customerTarget.name}
         </Typography>
+        {transactions
+          .filter((transaction) =>
+            customerTarget.transactions.includes(transaction.id),
+          )
+          .map((transaction) => {
+            return (
+              <Link href={`/transactions/${transaction.id}`}>
+                <Typography variant="h6" component="h2">
+                  transaction +{transaction.profit} by this user
+                </Typography>
+              </Link>
+            );
+          })}
       </Box>
+      <Typography variant="h3" component="h3">
+        Status: {customerTarget.status}
+      </Typography>
+      <Typography variant="h5" component="h4">
+        age: {customerTarget.age}
+      </Typography>
     </Box>
   );
 }
