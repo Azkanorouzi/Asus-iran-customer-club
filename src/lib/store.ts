ccc3da033f5105df6c491b4474fe3ecff0cba4cd
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-08-05 18:11:50 +0330

    Use zustand middleware to store dasta

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,17 +2,20 @@ import { customerData } from "@/data/customersData";
 import { transactionData } from "@/data/transactionsData";
 import { customerDataType, transactionDataType } from "@/types/definitions";
 import { createContext, useContext } from "react";
-import { createStore, useStore as useZustandStore } from "zustand";
+import {
+  createStore,
+  StateCreator,
+  useStore as useZustandStore,
+} from "zustand";
+import { persist } from "zustand/middleware"; // Import the persist middleware
 import { PreloadedStoreInterface } from "./StoreProvider";
 
 export interface StoreInterface {
   transactions: transactionDataType[];
   customers: customerDataType[];
-  // Customer
   deleteCustomer: (id: string) => void;
   editCustomer: (id: string, newCustomer: customerDataType) => void;
   addCustomer: (newCustomer: customerDataType) => void;
-  // Transactions
   deleteTransaction: (id: string) => void;
   editTransaction: (id: string, newTransaction: transactionDataType) => void;
   addTransaction: (transaction: transactionDataType) => void;
@@ -40,54 +43,65 @@ export function useStore<T>(selector: (state: StoreInterface) => T) {
 }
 
 export function initializeStore(preloadedState: PreloadedStoreInterface) {
-  return createStore<StoreInterface>((set, get) => ({
-    ...getDefaultInitialState(),
-    ...preloadedState,
-    // Customer methods
-    editCustomer: (id, newCustomer) => {
-      set(({ customers }) => {
-        const updatedCustomers = customers.map((customer) =>
-          customer.id === id ? newCustomer : customer,
-        );
-        return { customers: updatedCustomers };
-      });
-    },
-    addCustomer: (newCustomer) => {
-      set(({ customers }) => {
-        const updatedCustomers = [newCustomer, ...customers];
-        return { customers: updatedCustomers };
-      });
-    },
-    deleteCustomer: (id) => {
-      set(({ customers }) => {
-        const updatedCustomers = customers.filter(
-          (customer) => customer.id !== id,
-        );
-        return { customers: updatedCustomers };
-      });
-    },
-    // Transaction methods
-    editTransaction: (id, newTransaction) => {
-      set(({ transactions }) => {
-        const updatedTransactions = transactions.map((transaction) =>
-          transaction.id === id ? newTransaction : transaction,
-        );
-        return { transactions: updatedTransactions };
-      });
-    },
-    addTransaction: (newTransaction) => {
-      set(({ transactions }) => {
-        const updatedTransactions = [newTransaction, ...transactions];
-        return { transactions: updatedTransactions };
-      });
-    },
-    deleteTransaction: (id) => {
-      set(({ transactions }) => {
-        const updatedTransactions = transactions.filter(
-          (transaction) => transaction.id !== id,
-        );
-        return { transactions: updatedTransactions };
-      });
-    },
-  }));
+  const store = createStore<StoreInterface>(
+    persist(
+      (set, get) => ({
+        ...getDefaultInitialState(),
+        ...preloadedState,
+        editCustomer: (id: string, newCustomer: customerDataType) => {
+          set(({ customers }: { customers: customerDataType[] }) => {
+            const updatedCustomers = customers.map((customer) =>
+              customer.id === id ? newCustomer : customer,
+            );
+            return { customers: updatedCustomers };
+          });
+        },
+        addCustomer: (newCustomer: customerDataType) => {
+          set(({ customers }: { customers: customerDataType[] }) => {
+            const updatedCustomers = [newCustomer, ...customers];
+            return { customers: updatedCustomers };
+          });
+        },
+        deleteCustomer: (id: string) => {
+          set(({ customers }: { customers: customerDataType[] }) => {
+            const updatedCustomers = customers.filter(
+              (customer) => customer.id !== id,
+            );
+            return { customers: updatedCustomers };
+          });
+        },
+        editTransaction: (id: string, newTransaction: transactionDataType) => {
+          set(({ transactions }: { transactions: transactionDataType[] }) => {
+            const updatedTransactions = transactions.map((transaction) =>
+              transaction.id === id ? newTransaction : transaction,
+            );
+            return { transactions: updatedTransactions };
+          });
+        },
+        addTransaction: (newTransaction: transactionDataType) => {
+          set(({ transactions }: { transactions: transactionDataType[] }) => {
+            const updatedTransactions = [newTransaction, ...transactions];
+            return { transactions: updatedTransactions };
+          });
+        },
+        deleteTransaction: (id: string) => {
+          set(({ transactions }: { transactions: transactionDataType[] }) => {
+            const updatedTransactions = transactions.filter(
+              (transaction) => transaction.id !== id,
+            );
+            return { transactions: updatedTransactions };
+          });
+        },
+      }),
+      {
+        name: "my-app-store", // The key for the localStorage
+        partialize: (state: StoreInterface) => ({
+          customers: state.customers,
+          transactions: state.transactions,
+        }), // Specify what to persist
+      },
+    ) as StateCreator<StoreInterface, [], [], StoreInterface>, // Type assertion here
+  );
+
+  return store;
 }
