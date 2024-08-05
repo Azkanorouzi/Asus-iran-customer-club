1401cc1b20dd30aebc5577102ac8f78fb1016b62
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-08-05 11:53:24 +0330

    Add task bar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,13 @@
+import { BottomBar } from "@/components/dashboard/BottomBar";
 import LogoutButton from "@/components/LogoutButton";
+import { BottomNavigation } from "@mui/material";
 import React, { ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
-      <LogoutButton />
+      <BottomBar />
     </>
   );
 }
